2cb38309e3f57bab41606509fbd5654dfd864ff4
b000rg <33711099+b000rg@users.noreply.github.com>
2020-07-17 18:47:03 -0500

    completed MVP and stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,44 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+    constructor(l, w, h) {
+        this.length = l;
+        this.width = w;
+        this.height = h;
+    };
+
+    volume() {
+        return this.length * this.width * this.height;
+    };
+
+    surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    };
+};
+
+let cuboid = new CuboidMaker(4, 5, 5);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+class CubeMaker extends CuboidMaker {
+    constructor(side) {
+        super(side, side, side);
+        this.sideLength = side;
+    };
+
+    volume() {
+        return this.sideLength ** 3;
+    };
+
+    surfaceArea() {
+        return 6 * this.sideLength ** 2;
+    };
+};
+
+let cube = new CubeMaker(6);
+
+console.log(cube.volume());
+console.log(cube.surfaceArea());
--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// When the variable `internal` is invoked inside the nested function, the interpreter can't find a variable by that name inside the function, so it moves its search to the next scope up, which would be the context of the top level function, where it will find the variable contained there and use its value.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let counter = 0;
+  for (let i = 1; i <= num; i++) counter += i;
+  return counter;
+};
+
+console.log(summation(4));
